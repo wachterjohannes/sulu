30efefe0d316c3897d53121b806bbe8267eb8431
Johannes Wachter <johannes@sulu.io>
2026-09-21 14:57:04 +0200

    Add a RequestLog admin view
    Lists the requests an installation sent to an external service, with a detail overlay for the resolved chain, timing and credits. The registering extension provides the endpoints, the resource and the translations, and passes the detail route and the translation prefix as view options. Adds the list field transformers badge and duration.

--- src/Sulu/Bundle/AdminBundle/Resources/js/index.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/index.js
@@ -41,7 +41,9 @@ import {
     ColorFieldTransformer,
     HtmlFieldTransformer,
     IconFieldTransformer,
+    BadgeFieldTransformer,
     BytesFieldTransformer,
+    DurationFieldTransformer,
     ColumnListAdapter,
     listAdapterRegistry,
     listFieldTransformerRegistry,
@@ -129,6 +131,7 @@ import PreviewForm from './views/PreviewForm';
 import FormOverlayList from './views/FormOverlayList';
 import Subscription from './views/Subscription';
 import {setSubscriptionConfig} from './views/Subscription/subscriptionConfig';
+import RequestLog from './views/RequestLog';
 import {initializeJexl} from './utils/jexl';
 import {ExternalLinkTypeOverlay, linkOverlayRegistry, LinkTypeOverlay} from './containers/Link';
 import linkTypeRegistry from './containers/Link/registries/linkTypeRegistry';
@@ -215,6 +218,7 @@ function registerViews() {
         {disableDefaultSpacing: true, fullscreen: true}
     );
     viewRegistry.add('sulu_ai_platform.subscription', Subscription);
+    viewRegistry.add('sulu_admin.request_log', RequestLog);
 }
 
 function registerListAdapters() {
@@ -241,7 +245,9 @@ function registerListFieldFilterTypes() {
 
 function registerListFieldTransformers() {
     listFieldTransformerRegistry.add('array', new ArrayFieldTransformer());
+    listFieldTransformerRegistry.add('badge', new BadgeFieldTransformer());
     listFieldTransformerRegistry.add('bytes', new BytesFieldTransformer());
+    listFieldTransformerRegistry.add('duration', new DurationFieldTransformer());
     listFieldTransformerRegistry.add('date', new DateFieldTransformer());
     listFieldTransformerRegistry.add('time', new TimeFieldTransformer());
     listFieldTransformerRegistry.add('datetime', new DateTimeFieldTransformer());
